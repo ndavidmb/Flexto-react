67c207ffc9a076ef453409185d29dd79e2c3502d
David Montaño <nmontanob@slabcode.com>
2023-08-01 15:20:34 -0500

    (feat): Add change password and  licensePlate

--- src/auth/PublicRouter.tsx
+++ src/auth/PublicRouter.tsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { RecoveryPassword } from './components/RecoveryPassword'
 import { Register } from './components/Register'
 import { AuthWrapper } from './pages/AuthWrapper'
 import { Loading } from '../shared/components/Loading/Loading'
@@ -9,10 +8,6 @@ export const PublicRouter = () => {
   return (
     <Routes>
       <Route index element={<AuthWrapper />} />
-      <Route
-        path="recovery-password"
-        element={<RecoveryPassword />}
-      />
       <Route
         path="register"
         element={
