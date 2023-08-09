2b8c224b17cbfccbec3431d43340bf0dcef4b3c7
Cristian <meconios@outlook.com>
2023-08-08 20:39:22 -0500

    Home routers

--- src/auth/HomeRouter.tsx
+++ src/auth/HomeRouter.tsx
@@ -1,17 +1,13 @@
 import { useSelector } from 'react-redux'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { ActRouter } from '../act/ActRouter'
+import { ApartmentClientPage } from '../apartments/pages/ApartmentClientPage'
+import { AuthWaitApproved } from '../auth/pages/AuthWaitApproved'
+import { BookingRouter } from '../booking/BookingRouter'
 import { ClientRequestsWrapper } from '../client-requests/ClientRequestsWrapper'
 import { LoadingSvg } from '../shared/components/Loading/Loading'
 import { RootState } from '../shared/store/store'
 import { AdminRouter } from './AdminRouter'
-import { AuthWaitApproved } from '../auth/pages/AuthWaitApproved'
-import { BookingRouter } from '../booking/BookingRouter'
-import { ApartmentClientPage } from '../apartments/pages/ApartmentClientPage'
-<<<<<<< HEAD
-import { ActRouter } from '../act/ActRouter'
-=======
-import { PaymentOwnerPage } from '../payments/pages/PaymentOwnerPage'
->>>>>>> b8cdd232b538e7a31d6722878e911decd16484da
 
 export const HomeRouter = () => {
   const { role } = useSelector(
